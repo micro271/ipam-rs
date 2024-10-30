fb60355eb6ae65961ae548e76c5ada096489e033
Matias Zelaya <128877338+micro271@users.noreply.github.com>
2024-10-30 20:22:27 -0300

    first commit

---
+++ initdb.sql
@@ -0,0 +1,41 @@
+CREATE TYPE CREDENTIAL AS (
+    username VARCHAR,
+    password VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS networks (
+    id UUID PRIMARY KEY,
+    network VARCHAR NOT NULL,
+    available INTEGER NOT NULL,
+    used INTEGER NOT NULL,
+    total INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS offices (
+    id UUID PRIMARY KEY,
+    description VARCHAR,
+    address VARCHAR UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS devices (
+    ip VARCHAR NOT NULL,
+    description VARCHAR,
+    office_id UUID,
+    rack VARCHAR,
+    room VARCHAR,
+    status VARCHAR NOT NULL,
+    network_id UUID NOT NULL,
+    credential CREDENTIAL,
+    PRIMARY KEY (ip, network_id),
+    FOREIGN KEY (network_id) REFERENCES networks(id) ON DELETE CASCADE,
+    FOREIGN KEY (office_id) REFERENCES offices(id) ON DELETE SET NULL
+);
+
+CREATE TYPE ROLE AS ENUM ('Admin', 'Operator', 'Guest');
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID,
+    username VARCHAR,
+    password TEXT,
+    role ROLE
+);
