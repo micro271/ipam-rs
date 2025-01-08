52b18c665c4484b2f8e81d385fa8db039d43ecd6
Matias Zelaya <128877338+micro271@users.noreply.github.com>
2025-01-07 23:10:48 -0300

    The update handler in the network can execute only if there are no devices or subnets. We will create a clean handler to clean the children of the network, meaning if there are no subnets or devices, it will be executed

--- src/handler/network.rs
+++ src/handler/network.rs
@@ -1,9 +1,7 @@
-use std::net::IpAddr;
-
 use super::RepositoryType;
 use super::*;
 use crate::{
-    database::{repository::QueryResult, transaction::Transaction},
+    database::repository::QueryResult,
     models::{device::Device, network::*},
 };
 use entries::models::NetworkCreateEntry;
@@ -32,36 +30,12 @@ pub async fn update(
     Query(id): Query<Uuid>,
     Json(updater): Json<UpdateNetwork>,
 ) -> Result<QueryResult<Network>, ResponseError> {
-    let network_current = state
-        .get::<Network>(Some(HashMap::from([("id", id.into())])))
-        .await?
-        .remove(0);
-
-    let mut transaction = state.transaction().await.unwrap();
-    let network = updater.network;
-
-    transaction
-        .update::<Network, _>(updater, Some(HashMap::from([("id", id.into())])))
-        .await?;
-
-    if network.is_some_and(|x| x != network_current.network) {
-        let tmp = network.unwrap();
-        let mut devices = state
-            .get::<Device>(Some(HashMap::from([(
-                "network_id",
-                network_current.network.into(),
-            )])))
-            .await?;
-        devices.sort_by_key(|x| x.ip);
-        let host = tmp.hosts().collect::<Vec<IpAddr>>();
-        for (pos, h) in host.into_iter().enumerate() {
-            transaction
-                .update::<Device, _>(h, Some(HashMap::from([("ip", devices[pos].ip.into())])))
-                .await?;
-        }
+    
+    if updater.network.is_some() && state.get::<Device>(Some(HashMap::from([("network_id",id.into())]))).await.is_ok() {
+        Err(ResponseError::builder().detail("The network have devices".to_string()).build())
+    } else {
+        Ok(state.update(updater, Some(HashMap::from([("id", id.into())]))).await?)
     }
-
-    Ok(QueryResult::Update(50))
 }
 
 pub async fn delete(
